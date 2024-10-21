91c1e63269aee9527586c38b80ea6371a6716cab
Paula Mallol <paula@checklyhq.com>
2024-10-21 15:41:25 +0200

    chore: update packages

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { v4: uuidv4 } = require('uuid');
 const morgan = require('morgan')
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5001
 
 app.use(express.json())
 app.use(cors())
