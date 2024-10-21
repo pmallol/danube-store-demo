e2ff85d6ea7f5efc88cea167869a0cba45e8e0d0
Paula Mallol <paula@checklyhq.com>
2024-10-21 15:43:26 +0200

    refactor: update tracing file and checkly config

--- checkly.config.ts
+++ checkly.config.ts
@@ -2,15 +2,16 @@ import { defineConfig } from 'checkly'
 import { Frequency } from 'checkly/constructs'
 
 export default defineConfig({
- projectName: 'Website Monitoring',
- logicalId: 'website-monitoring-1',
- repoUrl: 'https://github.com/acme/website',
- checks: {
+  projectName: 'Danube DEMO Local',
+  logicalId: 'danube-demo-local-1',
+  repoUrl: 'https://github.com/acme/website',
+  checks: {
    activated: true,
    muted: false,
-   runtimeId: '2022.10',
+    runtimeId: '2024.02',
    frequency: Frequency.EVERY_5M,
-   locations: ['us-east-1', 'eu-west-1'],
+    //  locations: ['us-east-1', 'eu-west-1'],
+    privateLocations: ['paulas-macbook-pro'],
    tags: ['website', 'api'],
    checkMatch: '**/__checks__/**/*.check.ts',
    ignoreDirectoriesMatch: [],
--- tracing.js
+++ tracing.js
@@ -1,28 +1,18 @@
-const { BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base')
-const { Resource } = require('@opentelemetry/resources')
 const { NodeSDK } = require('@opentelemetry/sdk-node')
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http')
-const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions')
-const {
-  getNodeAutoInstrumentations,
-} = require('@opentelemetry/auto-instrumentations-node')
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-proto')
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node')
+const { BatchSpanProcessor, SamplingDecision } = require('@opentelemetry/sdk-trace-base')
+const { trace } = require('@opentelemetry/api')
 
 const exporter = new OTLPTraceExporter({
   timeoutMillis: 2000,
-  url: process.env.OTEL_URL,
+  url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
   headers: {
-   Authorization: process.env.OTEL_TOKEN
+    Authorization: process.env.OTEL_TOKEN
   },
 })
 
 const sdk = new NodeSDK({
-  resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: 'danube-api',
-    'cx.application.name': 'danube-api',
-    'cx.subsystem.name': 'express',
-  }),
-  traceExporter: exporter,
-  spanProcessor: new BatchSpanProcessor(exporter),
   instrumentations: [getNodeAutoInstrumentations({
     '@opentelemetry/instrumentation-hapi': {
       enabled: false,
@@ -40,15 +30,24 @@ const sdk = new NodeSDK({
       enabled: true,
     },
   })],
+  spanProcessors: [new BatchSpanProcessor(exporter)],
+  sampler: {
+    shouldSample: (context, traceId, spanName, spanKind, attributes, links) => {
+      const isChecklySpan = trace.getSpan(context)?.spanContext()?.traceState?.get('checkly')
+      if (isChecklySpan) {
+        return { decision: SamplingDecision.RECORD_AND_SAMPLED }
+      } else {
+        return { decision: SamplingDecision.NOT_RECORD }
+      }
+    },
+  },
 })
+
 sdk.start()
 
-process.on("SIGTERM", () => {
-  sdk
-      .shutdown()
-      .then(
-          () => console.log("SDK shut down successfully"),
-          (err) => console.log("Error shutting down SDK", err)
-      )
-      .finally(() => process.exit(0));
-});
+process.on('SIGTERM', () => {
+  sdk.shutdown()
+    .then(() => console.log('OTel Tracing terminated'))
+    .catch((error) => console.log('Error terminating OTel tracing', error))
+    .finally(() => process.exit(0))
+})
